cf4fe2a3b08711355b5ca77859fc409bad4a843f
G Roques <groques360@gmail.com>
2020-05-24 21:03:18 -0500

    v0.1.0a2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/gbroques/ose-workbench-platform',
     author='G Roques',
-    version='0.1.0a1',
+    version='0.1.0a2',
     packages=['osewb'],
     entry_points={
         'console_scripts': [
