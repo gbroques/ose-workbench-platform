303232755f4ab2108714f16e853459a861f13b52
G Roques <groques360@gmail.com>
2020-06-14 08:36:47 -0500

    Add OSE logo sticker to docs

--- docs/conf.py
+++ docs/conf.py
@@ -111,6 +111,13 @@ autodoc_mock_imports = [
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'style_nav_header_background': '#498AAA',
+    'logo_only': False
+}
+
+html_logo = './_static/ose-logo.jpg'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
