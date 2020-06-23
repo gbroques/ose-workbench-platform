6f85dd8a051a9a610e5604bba146aaa83964fcf8
G Roques <groques360@gmail.com>
2020-06-22 20:08:57 -0500

    Update coverage command to work with workbenches and ose-workbench-core (Closes #29)

--- osewb/handle_test_command.py
+++ osewb/handle_test_command.py
@@ -1,5 +1,6 @@
 
 import os
+from pathlib import Path
 
 from .execute_command import execute_command
 
@@ -9,7 +10,8 @@ def handle_test_command(base_package: str,
                         with_coverage: bool = False) -> None:
     test_command = 'pytest --color=yes'
     if with_coverage:
-        test_command += ' --cov {}'.format(base_package)
+        cov_pkg = find_coverage_package(root_of_git_repository, base_package)
+        test_command += ' --cov {}'.format(cov_pkg)
     test_dir = os.path.join(root_of_git_repository, 'tests')
     test_command += ' ' + test_dir
     execute_command(test_command)
@@ -18,3 +20,23 @@ def handle_test_command(base_package: str,
         print('Coverage report generated in htmlcov/ directory.')
         print('To view, open htmlcov/index.html in a web browser, or run:\n')
         print('    osewb browse coverage\n')
+
+
+def find_coverage_package(root_of_git_repository: str, base_package: str) -> str:
+    if is_workbench(root_of_git_repository):
+        library_pkg = os.path.join(root_of_git_repository, base_package)
+        if not Path(library_pkg).exists():
+            print('library package not found')
+            exit(1)
+        return os.path.join(root_of_git_repository, base_package)
+    else:  # Assume library with base package -> app / gui sub-packages
+           # this assumption currently only holds true for ose-workbench-core
+        app_pkg = os.path.join(root_of_git_repository, base_package, 'app')
+        if not Path(app_pkg).exists():
+            print('app package not found')
+            exit(1)
+        return app_pkg
+
+
+def is_workbench(root_of_git_repository: str) -> bool:
+    return Path(root_of_git_repository).joinpath('freecad').exists()
