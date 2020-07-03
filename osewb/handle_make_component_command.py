42557119bb66b55e0ee4963c52fd87517e7bb796
G Roques <groques360@gmail.com>
2020-07-03 10:50:33 -0500

    Refactor and clean-up handle_make_component_command

--- osewb/handle_make_component_command.py
+++ osewb/handle_make_component_command.py
@@ -3,6 +3,7 @@ import inspect
 import os
 import re
 from pathlib import Path
+from typing import List
 
 from isort import SortImports
 from jinja2 import Environment, PackageLoader
@@ -16,23 +17,29 @@ def handle_make_component_command(base_package: str,
         env = Environment(
             loader=PackageLoader('osewb', 'templates'))
         template = env.get_template('part.py')
-        part_package_path = Path(os.path.join(
+
+        # 1. Construct component package
+        component_package_path = Path(os.path.join(
             root_of_git_repository,
             base_package,
-            'part'
+            component
         ))
-        part_package_path.mkdir(exist_ok=True)
-        part_package_init_module_path = part_package_path.joinpath(
-            '__init__.py')
-        part_lower = name.lower()
-        new_part_package = '_' + part_lower
-        new_part_package_path = Path(
-            part_package_path).joinpath(new_part_package)
-        if(new_part_package_path.exists()):
+        component_package_path.mkdir(exist_ok=True)
+
+        # 2. Construct new component sub-package
+        #    Stop if it exists
+        new_component_sub_package = '_' + name.lower()
+        new_component_sub_package_path = Path(
+            component_package_path).joinpath(new_component_sub_package)
+        if(new_component_sub_package_path.exists()):
             print('{} package already exists. Skipping creation.'.format(
-                new_part_package))
+                new_component_sub_package))
             return None
-        import_statement = 'from ._{} import {}\n'.format(part_lower, name)
+        new_component_sub_package_path.mkdir(exist_ok=True)
+
+        # 3. Update init module of component package
+        part_package_init_module_path = component_package_path.joinpath(
+            '__init__.py')
         with part_package_init_module_path.open('r+') as f:
             lines = f.readlines()
             text = ''.join(lines)
@@ -45,30 +52,29 @@ def handle_make_component_command(base_package: str,
                 return None
             else:
                 all_members = eval(match.group(1))
-                next_all_members = all_members + [name]
-                sorted_next_all_members = sorted(next_all_members)
-                next_all_members_with_indentation = [
-                    "    '{}'".format(m) for m in sorted_next_all_members]
-                next_all_members_string = ',\n'.join(
-                    next_all_members_with_indentation)
-                next_all_string = '__all__ = [\n{}\n]'.format(
-                    next_all_members_string)
-                ret = re.sub(all_pattern, next_all_string, text)
-                ret += '\n' + import_statement
+                next_all_string = build_next_all_string(all_members, name)
+                new_contents = re.sub(all_pattern, next_all_string, text)
+                import_statement = 'from ._{} import {}\n'.format(
+                    name.lower(), name)
+                new_contents += '\n' + import_statement
                 f.seek(0)
-                f.write(ret)
+                f.write(new_contents)
         SortImports(part_package_init_module_path.resolve())
 
-        new_part_package_path.mkdir(exist_ok=True)
-        init_module_path = new_part_package_path.joinpath('__init__.py')
+        # 4. Setup init module in component sub-package
+        init_module_path = new_component_sub_package_path.joinpath(
+            '__init__.py')
         init_module_path.touch(exist_ok=True)
-        import_statement = 'from .{} import {}\n'.format(part_lower, name)
-        all_declaration = "__all__ = ['{}']".format(name)
         with init_module_path.open('a') as f:
+            import_statement = 'from .{} import {}\n'.format(
+                name.lower(), name)
+            all_declaration = "__all__ = ['{}']".format(name)
             f.write('\n'.join([import_statement, all_declaration]))
         SortImports(init_module_path.resolve())
-        module_name = '{}.py'.format(part_lower)
-        part_module_path = new_part_package_path.joinpath(module_name)
+
+        # 5. Create new component module using template
+        module_name = '{}.py'.format(name.lower())
+        part_module_path = new_component_sub_package_path.joinpath(module_name)
         part_module_existed_before = part_module_path.exists()
         part_module_path.touch(exist_ok=True)
         if part_module_existed_before:
@@ -76,3 +82,14 @@ def handle_make_component_command(base_package: str,
                 module_name))
             return None
         part_module_path.write_text(template.render(name=name) + '\n')
+
+
+def build_next_all_string(all_members: List[str], name: str) -> str:
+    next_all_members = all_members + [name]
+    sorted_next_all_members = sorted(next_all_members)
+    next_all_members_with_indentation = [
+        "    '{}'".format(m) for m in sorted_next_all_members]
+    next_all_members_string = ',\n'.join(
+        next_all_members_with_indentation)
+    return '__all__ = [\n{}\n]'.format(
+        next_all_members_string)
