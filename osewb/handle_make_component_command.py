273b2a26aec5307ba49fbf8f25b54b154678bdb0
G Roques <groques360@gmail.com>
2020-07-03 10:14:32 -0500

    Update make part command to use new pattern

--- osewb/handle_make_component_command.py
+++ osewb/handle_make_component_command.py
@@ -1,4 +1,7 @@
+import importlib
+import inspect
 import os
+import re
 from pathlib import Path
 
 from isort import SortImports
@@ -22,17 +25,47 @@ def handle_make_component_command(base_package: str,
         part_package_init_module_path = part_package_path.joinpath(
             '__init__.py')
         part_lower = name.lower()
-        import_statement = 'from .{} import {}\n'.format(part_lower, name)
-        with part_package_init_module_path.open('a') as f:
-            f.write(import_statement)
-        SortImports(part_package_init_module_path.resolve())
+        new_part_package = '_' + part_lower
         new_part_package_path = Path(
-            part_package_path).joinpath(part_lower)
+            part_package_path).joinpath(new_part_package)
+        if(new_part_package_path.exists()):
+            print('{} package already exists. Skipping creation.'.format(
+                new_part_package))
+            return None
+        import_statement = 'from ._{} import {}\n'.format(part_lower, name)
+        with part_package_init_module_path.open('r+') as f:
+            lines = f.readlines()
+            text = ''.join(lines)
+            white_space = '[\s\S]+'
+            all_pattern = white_space.join(['__all__', '=', '(\[', '\])'])
+            match = re.search(all_pattern, text)
+            if not match:
+                print('No __all__ found in {}'.format(
+                    part_package_init_module_path.resolve()))
+                return None
+            else:
+                all_members = eval(match.group(1))
+                next_all_members = all_members + [name]
+                sorted_next_all_members = sorted(next_all_members)
+                next_all_members_with_indentation = [
+                    "    '{}'".format(m) for m in sorted_next_all_members]
+                next_all_members_string = ',\n'.join(
+                    next_all_members_with_indentation)
+                next_all_string = '__all__ = [\n{}\n]'.format(
+                    next_all_members_string)
+                ret = re.sub(all_pattern, next_all_string, text)
+                ret += '\n' + import_statement
+                f.seek(0)
+                f.write(ret)
+        SortImports(part_package_init_module_path.resolve())
+
         new_part_package_path.mkdir(exist_ok=True)
         init_module_path = new_part_package_path.joinpath('__init__.py')
         init_module_path.touch(exist_ok=True)
+        import_statement = 'from .{} import {}\n'.format(part_lower, name)
+        all_declaration = "__all__ = ['{}']".format(name)
         with init_module_path.open('a') as f:
-            f.write(import_statement)
+            f.write('\n'.join([import_statement, all_declaration]))
         SortImports(init_module_path.resolve())
         module_name = '{}.py'.format(part_lower)
         part_module_path = new_part_package_path.joinpath(module_name)
